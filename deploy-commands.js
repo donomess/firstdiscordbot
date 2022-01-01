487dd0193d698c25913bd74cb3ec566857931d37
donomess <donovanmess01@gmail.com>
2022-01-01 18:24:40 -0500

    added ping and dynamic command access.

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,17 +3,13 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [
-    new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-    new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-    new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-    new SlashCommandBuilder().setName('add').setDescription('Adds two numbers!'),
-    new SlashCommandBuilder().setName('sub').setDescription('Subtracts two numbers!'),
-    new SlashCommandBuilder().setName('mul').setDescription('Multiples two numbers!'),
-    new SlashCommandBuilder().setName('div').setDescription('Divides two numbers!'),
+const commands = []
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-]
-    .map(command => command.toJSON());
+for(const file of commandFiles){
+    const command = require('./commands/${file}');
+    commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9'}).setToken(token);
 
--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('fs');
 
 //Bot instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commmands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+    const command = require('./commands/${file}');
+    //Set a new item in the collection
+    //With the key as the command name and the value as the exported module.
+    client.commands.set(command.data.name, command);
+}
 
 client.once('ready', () => {
     console.log('Bot Ready.');
 });
+
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isCommand()) return;
+
+    const {commandName} = interaction; 
+
+
+})
 
 
 //Login (Should be last thing)
