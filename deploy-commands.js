b6512d87908bd2afc18b77bf121e28dffd8f9c39
donomess <donovanmess01@gmail.com>
2022-01-01 18:17:17 -0500

    added deploy command file. only needs to be run when adding or editing commands.

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,9 +7,16 @@ const commands = [
     new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
     new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
     new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+    new SlashCommandBuilder().setName('add').setDescription('Adds two numbers!'),
+    new SlashCommandBuilder().setName('sub').setDescription('Subtracts two numbers!'),
+    new SlashCommandBuilder().setName('mul').setDescription('Multiples two numbers!'),
+    new SlashCommandBuilder().setName('div').setDescription('Divides two numbers!'),
+
 ]
     .map(command => command.toJSON());
 
 const rest = new REST({ version: '9'}).setToken(token);
 
-rest.put(Routes.applicationGuildCommands)
+rest.put(Routes.applicationGuildCommands(clientId, guildId), {body: commands})
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
