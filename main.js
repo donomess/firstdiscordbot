9e36902f44ddd7303f70fc3b5fdbcc2ad2b83905
donomess <donovanmess01@gmail.com>
2022-01-01 18:28:04 -0500

    added command handler to main.

--- main.js
+++ main.js
@@ -21,10 +21,17 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
     if(!interaction.isCommand()) return;
 
-    const {commandName} = interaction; 
+    const command = client.commands.get(interaction.commandName);
 
+    if(!command) return;
 
-})
+    try{
+        await command.execute(interaction);
+    } catch (error){
+        console.error(error);
+        await interaction.reply({content : 'There was an error while executing this commands!', ephemeral: true});
+    }
+});
 
 
 //Login (Should be last thing)
