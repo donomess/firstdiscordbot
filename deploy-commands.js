bba18f98a587d86ccb7621f88d17344975aaca65
donomess <donovanmess01@gmail.com>
2022-01-01 18:25:25 -0500

    changed an include.

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
